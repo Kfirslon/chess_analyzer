e43419990bc2180d190882850d76e67c07d191cf
Kfirslon <kfirslon@gmail.com>
2025-06-19 22:32:34 -0400

    Update chess_app.py

--- chess_app.py
+++ chess_app.py
@@ -3,9 +3,9 @@ from chess_analyzer import run_analysis
 
 st.title("♟️ Kfir - Chess.com Game Analyzer")
 
-username = st.text_input("Enter Chess.com username", value="kfirslon9")
+username = st.text_input("Enter Chess.com username", value="username")
 year = st.number_input("Enter year", min_value=2000, max_value=2025, value=2025)
-months_input = st.text_input("Enter months (comma-separated, e.g., 1,2,3)", value="1,2,3,4,5")
+months_input = st.text_input("Enter months (comma-separated, e.g., 1,2,3)", value="1")
 
 if st.button("Run Analysis"):
     try:
